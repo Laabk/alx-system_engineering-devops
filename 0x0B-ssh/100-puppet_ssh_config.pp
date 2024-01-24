da80a30049c071b035f364e5ba0bb1561f57495d
laabk <laabkdavid@gmail.com>
2024-01-24 09:33:03 -0800

    shh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Puppet script to create ssh config file
+file_line { 'password requirement off':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
