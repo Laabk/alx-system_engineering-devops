4349b3361c94dd468e0c77b96ffebd697f70b602
laabk <laabkdavid@gmail.com>
2024-03-05 05:33:30 -0800

    api_advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,25 +3,26 @@
 the top ten hot posts of a subreddit"""
 
 import requests as req
+after = None
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """ Queries to Reddit API """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "Google Chrome Version 81.0.4044.129"
-        }
-    param = {
-        "after": after,
-        "limit": 100,
-    }
-    resp = req.get(url, headers=headers, params=param, allow_redirects=False)
-    if resp.status_code == 404:
-        return None
+def recurse(subreddit, hot_list=[]):
+    """returning top ten post titles recursively"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    param = {'after': after}
+    resp = req.get(url, params=param, headers=user_agent,
+            allow_redirects=False)
+
+    if resp.status_code == 200:
+        afr_dta = resp.json().get("data").get("after")
+        if afr_dta is not None:
+            after = afr_dta
+            recurse(subreddit, hot_list)
+        al_ttle = resp.json().get("data").get("children")
+        for titl in al_ttle:
+            hot_list.append(titl.get("data").get("title"))
+        return hot_list
     else:
-        pos = resp.json().get("data").get("children")
-        h_list += [post.get("data").get("title") for post in pos]
-        after = resp.json().get("data").get("after")
-        if after is not None:
-                recurse(subreddit, h_list, after)
-        return h_list
+        return (None)
