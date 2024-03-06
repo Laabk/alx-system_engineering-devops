d1807ff07a003aaa372beddb980789a5ea2ca949
laabk <laabkdavid@gmail.com>
2024-03-06 14:37:52 -0800

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# this fiixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`observed.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
